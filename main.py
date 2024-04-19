6c280fb1b934608da270300670ec5b5f887e058a
Higor Batista <161776354+Hiigorx@users.noreply.github.com>
2024-04-18 22:41:24 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,40 +1,3 @@
-#Calculadora de Notas
-
-#qtdNotas = int(input("Digite a quantidade de notas desejadas: "))
-#soma = 0
-#contador = 0
-#
-#while qtdNotas > 0:
-#    notas = int(input("Digite a nota: "))
-#    contador = contador + 1
-#    soma = soma + notas
-#    qtdNotas = qtdNotas - 1
-#
-#media = soma / contador
-#print(f'A média das notas é de: {media}')
-
-
-
-
-
-#media = 0
-#soma = 0
-#contador = 0
-#
-#while True:
-#
-#    selecao = int(input("Digite um número inteiro: "))
-#    if selecao == 0:
-#        break
-#    contador = contador + 1
-#    soma = soma + selecao
-#    media = soma / contador
-#
-#print(f'A soma dos valores digitados é de: {soma:.2f}\nA média aritmética dos números digitados é de: {media:.2f}')
-#print("Fim da execução.")
-
-
-
 produto1 = 0
 produto2 = 0
 produto3 = 0
@@ -52,6 +15,13 @@ cod2 = 1.00
 cod3 = 4.00
 cod5 = 7.00
 cod9 = 8.00
+
+
+pgt1 = "DÉBITO"
+pgt2 = "CRÉDITO"
+pgt3 = "PIX"
+pgt4 = "BOLETO BANCÁRIO"
+
 print('''Código -------- Preço 
    1   --------  0,50
    2   --------  1,00
@@ -75,13 +45,118 @@ while True:
     elif pedido == 9:
         produto9 = cod9
         qtd9 = int(input("Digite a quantidade desejada do produto selecionado: "))
+    elif pedido == 0:
+        soma = (produto1 * qtd1) + (produto2 * qtd2) + (produto3 * qtd3) + (produto5 * qtd5) + (produto9 * qtd9)
+        print(f"O valor da sua compra foi de R${soma}.")
+        pgt = int(input('''---------- FORMAS DE PAGAMENTO ACEITAS ----------
+        [1] DÉBITO
+        [2] CRÉDITO
+        [3] PIX
+        [4] BOLETO BANCÁRIO
+        
+        DEFINA A FORMA DE PAGAMENTO UTILIZADA: '''))
+
+        if pgt == 1:
+            print(f"Você selecionou {pgt1} como forma de pagamento para a sua compra de R${soma}.")
+            conclusao = int(input('''Você deseja encerrar a sua compra?
+            [1] SIM
+            [2] NÃO
+            '''))
+
+            test = 1,2
+            if conclusao == 1:
+                print("Obrigado pela sua compra, até a próxima!")
+                break
+            elif conclusao == 2:
+                continue
+            while conclusao !=  test:
+                final = int(input('''Você deseja encerrar a sua compra?
+            [1] SIM
+            [2] NÃO
+            '''))
+                if final == 1:
+                    print("Obrigado pela sua compra, até a próxima!")
+                    break
+
+                else:
+                    continue
+        elif pgt == 2:
+            print(f"Você selecionou {pgt2} como forma de pagamento para a sua compra de R${soma}.")
+            conclusao = int(input('''Você deseja encerrar a sua compra?
+            [1] SIM
+            [2] NÃO
+            '''))
+
+            test = 1,2
+            if conclusao == 1:
+                print("Obrigado pela sua compra, até a próxima!")
+                break
+            elif conclusao == 2:
+                continue
+            while conclusao !=  test:
+                final = int(input('''Você deseja encerrar a sua compra?
+            [1] SIM
+            [2] NÃO
+            '''))
+                if final == 1:
+                    print("Obrigado pela sua compra, até a próxima!")
+                    break
+
+                else:
+                    continue
+        elif pgt == 3:
+            print(f"Você selecionou {pgt3} como forma de pagamento para a sua compra de R${soma}.")
+            conclusao = int(input('''Você deseja encerrar a sua compra?
+            [1] SIM
+            [2] NÃO
+            '''))
+
+            test = 1,2
+            if conclusao == 1:
+                print("Obrigado pela sua compra, até a próxima!")
+                break
+            elif conclusao == 2:
+                continue
+            while conclusao !=  test:
+                final = int(input('''Você deseja encerrar a sua compra?
+            [1] SIM
+            [2] NÃO
+            '''))
+                if final == 1:
+                    print("Obrigado pela sua compra, até a próxima!")
+                    break
+
+                else:
+                    continue
+        elif pgt == 4:
+            print(f"Você selecionou {pgt4} como forma de pagamento para a sua compra de R${soma}.")
+            conclusao = int(input('''Você deseja encerrar a sua compra?
+            [1] SIM
+            [2] NÃO
+            '''))
+
+            test = 1, 2
+            if conclusao == 1:
+                print("Obrigado pela sua compra, até a próxima!")
+                break
+            elif conclusao == 2:
+                continue
+            while conclusao != 1 or conclusao != 2:
+                conclusao = int(input('''Você deseja encerrar a sua compra?
+            [1] SIM
+            [2] NÃO
+            '''))
+                if conclusao == 1:
+                    print("Obrigado pela sua compra, até a próxima!")
+                    break
+
+                else:
+                    continue
+
     else:
         print("Codigo invalido.")
-        break
+        continue
 
 
 
-    if pedido == 0:
-        soma = (produto1 * qtd1) + (produto2 * qtd2) + (produto3 * qtd3) + (produto5 * qtd5) + (produto9 * qtd9)
-        break
-        print(soma)
+
